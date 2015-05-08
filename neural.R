ade664fc3d2551c40c35dc5136f73a6e598f62a6
Kaj Hejlesen <kajhejlesen@gmail.com>
2015-05-08 18:03:37 +0200

    ...

--- neural.R
+++ neural.R
@@ -11,49 +11,73 @@ data_filtered$country_txt <- factor(data_filtered$country_txt)
 
 countries <- levels(data_filtered$country_txt)
 
-
-generate_input_frame <- function(data, start, end, by) {
-  total <- data.frame()
-  
-  for (year in seq(from = start,to = end, by = by)) {
-    data_test <- subset(data, subset = iyear >= year & iyear < year+5)
-    data_result <- subset(data, subset = iyear >= year+5 & iyear < year+7)
-    input <- data.table(as.data.frame(table(data_test$country_txt, data_test$iyear, data_test$attacktype1_txt)))
-    input <- input[, j=sum(Freq), by = list(Var1, Var3)]
-    input$start_year <- year
-    input <- reshape(input, timevar = c("Var3"), idvar = c("Var1","start_year"), direction = "wide")
-    
-    
-    result <- data.frame(table(data_result$country_txt, data_result$nkill > 10))
-    result$is_true <- ifelse(test = result$Var2 == TRUE & result$Freq > 0, 1,0)
-    result <- result[152:nrow(result),]
-    
-    input$result <- res$is_true
-    
-    total <- rbind(total, input)
-    
-  }
-  setnames(total, colnames(total), c("country", "start_year", "x1","x2","x3","x4","x5","x6","x7","x8","x9","result"))
-  
-  total
+# alternative normalization function, not used
+norm.fun = function(x){
+  (x - min(x))/(max(x) - min(x)) 
 }
 
 
-training_set <- generate_input_frame(data_filtered, 1970,2006,4)
-test_set <- generate_input_frame(data_filtered, 1972,2004,4)
+
+total <- data.frame()
+time_series <- seq(from = 1970, to = 2006, by = 2)
+
+# removing result that contain 1993 as we have no data for that year
+time_series <- time_series[!time_series %in% c(1988)]
+
+for (year in time_series) {
+  
+  data_test <- subset(data_filtered, subset = iyear >= year & iyear < year+5)
+  data_result <- subset(data_filtered, subset = iyear >= (year+5) & iyear < (year+7))
+  input <- data.table(as.data.frame(table(data_test$country_txt, data_test$iyear, data_test$attacktype1_txt)))
+  input <- input[, j=sum(Freq), by = list(Var1, Var3)]
+  input$start_year <- year
+  input <- reshape(input, timevar = c("Var3"), idvar = c("Var1","start_year"), direction = "wide")
+  
+  # generating data for average fatalities per incident
+  kill_rate_country <- data_test[, j=mean(nkill, na.rm=TRUE), by = list(country_txt)]
+  data_test.keys <- data_filtered[,CJ(country_txt=unique(country_txt))]
+  setkey(kill_rate_country, country_txt)
+  kill_rate_country <- kill_rate_country[data_test.keys]
+  kill_rate_country[is.na(kill_rate_country)] <- 0
+  input$killrate <- kill_rate_country$V1
+  
+  
+  
+  result <- data.frame(table(data_result$country_txt, data_result$nkill > 10))
+  result$is_true <- ifelse(test = result$Var2 == TRUE & result$Freq > 0, 1,0)
+  result <- result[152:nrow(result),]
+  
+  input$result <- res$is_true
+  
+  
+  total <- rbind(total, input)  
+}
+
+setnames(total, colnames(total), c("country", "start_year", "Assassination","Hijacking","Kidnapping","Barricade","Bombing","Unknown","Armed.Assault","Unarmed.Assault","Facility","Kill.Rate", "result"))
 
 
-form.in<-as.formula('result~x1+x2+x3+x4+x5+x6+x7+x8+x9')
+data_set <- data.table(apply(total[,c(4:length(total)-1), with=FALSE], 2, scale))
+data_set$result <- total$result
 
-mod2<-neuralnet(form.in,data=training_set,hidden= c(10), linear.output = FALSE, lifesign="minimal", lifesign.step = 100, stepmax = 1E6)
+trainingsamples <- sample(1:nrow(data_set), nrow(data_set)/2)
 
+form.in<-as.formula('result~Assassination + Hijacking + Kidnapping + Barricade + Bombing + Unknown + Armed.Assault + Unarmed.Assault + Facility + Kill.Rate')
+
+mod2<-neuralnet(form.in,data=data_set[trainingsamples],hidden= c(8), linear.output = FALSE, lifesign="minimal", lifesign.step = 100, stepmax = 1E5, rep = 5)
 plot(mod2)
 
+test_set <- data_set[!trainingsamples]
 
-prediction <- compute(mod2, test_set[,3:11, with = FALSE])
+prediction <- compute(mod2, test_set[,1:10, with = FALSE])
 
-test_set$prediction <- prediction
+test_set$prediction <- prediction$net.result
+test_set$prediction.round <- ifelse(test_set$prediction >= 0.5, 1,0)
 
+confusion_matrix <- table(test_set$prediction.round, test_set$result)
+
+confusion_matrix
+
+plot.nnet(mod2)
 
 result_name <- as.data.frame(cbind(countries, as.vector(mod2$net.result[[1]])))
 result_name$V2 <- as.numeric(as.character(result_name$V2))
@@ -64,4 +88,3 @@ mapCountryData(mapToPlot = map, nameColumnToPlot = "V2", numCats = 9,
                catMethod = "diverging", colourPalette = brewer.pal(9, "Reds"),
                missingCountryCol = "grey90", oceanCol = "lightsteelblue1", addLegend = T)
 
-mod2$
